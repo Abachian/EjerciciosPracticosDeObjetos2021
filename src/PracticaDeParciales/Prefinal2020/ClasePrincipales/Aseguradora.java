07e59bfa2d859d76651326652f9870dcb4556621
Abachian <54768966+Abachian@users.noreply.github.com>
2021-05-27 18:38:26 -0300

    Terminado Prefinal 2020

--- src/PracticaDeParciales/Prefinal2020/ClasePrincipales/Aseguradora.java
+++ src/PracticaDeParciales/Prefinal2020/ClasePrincipales/Aseguradora.java
@@ -1,6 +1,7 @@
 package PracticaDeParciales.Prefinal2020.ClasePrincipales;
 
 import PracticaDeParciales.Prefinal2020.Filtros.Filtro;
+import PracticaDeParciales.Prefinal2020.Filtros.FiltroDescripcionExacta;
 import PracticaDeParciales.Prefinal2020.Filtros.FiltroPorPalabra;
 
 import java.time.LocalDate;
@@ -33,13 +34,13 @@ public class Aseguradora {
     public static void main(String[] args) {
         Aseguradora A= new Aseguradora("LaSegunda");
         LocalDate f1= LocalDate.of(1998,2,25);
-        SeguroAutomotorVida sa1= new SeguroAutomotorVida(414,"Toyota", 15000, 4, 25, 300);
+        SeguroAutomotorVida sa1= new SeguroAutomotorVida(414,"Toyotal", 15000, 4, 25, 300);
         SeguroDecremental s1d= new SeguroDecremental(412, "Toyota", 800, 3,f1,30);
         SeguroIntegrador si= new SeguroIntegrador(41438762);
         si.agregarSeguro(sa1);
         si.agregarSeguro(s1d);
         A.agregarSeguro(si);
-        FiltroPorPalabra filtro= new FiltroPorPalabra("Toyota");
+        FiltroDescripcionExacta filtro= new FiltroDescripcionExacta("Toyota");
         System.out.println(A.listarSeguros(filtro));
     }
 }
--- src/PracticaDeParciales/Prefinal2020/Filtros/FiltroDescripcionExacta.java
+++ src/PracticaDeParciales/Prefinal2020/Filtros/FiltroDescripcionExacta.java
@@ -1,4 +1,18 @@
 package PracticaDeParciales.Prefinal2020.Filtros;
 
-public class FiltroDescripcionExacta {
+import PracticaDeParciales.Prefinal2020.ClasePrincipales.Seguro;
+
+public class FiltroDescripcionExacta implements Filtro {
+    private String descrpicionBuscada;
+
+    public FiltroDescripcionExacta(String descrpicionBuscada) {
+        this.descrpicionBuscada = descrpicionBuscada;
+    }
+
+    @Override
+    public boolean cumple(Seguro s) {
+        if (s.getDescripcion().equals(descrpicionBuscada))
+            return true;
+        return false;
+    }
 }
